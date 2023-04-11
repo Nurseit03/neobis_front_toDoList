7d1e0385ae4bb8c3ce56accb578a9b8b237241ed
Nurseit <nurseitbakytbekov@gmail.com>
2023-04-12 00:00:51 +0600

    add recursive prompt

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-let userName = prompt("Как вас зовут?");
-document.getElementById("user__name").textContent = userName;
+function changeUserName(){
+    document.getElementById("user__name").textContent = userName;
+}
+function recursivePrompt() {
+    let userName = prompt("Как вас зовут?");
+    
+    if (userName === null || userName === "") {
+      recursivePrompt();
+    } else {
+      changeUserName();
+    }
+  }
+  
+  recursivePrompt();
