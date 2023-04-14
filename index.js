db271f356cd98c3b70de58270ab61876463eb09b
Nurseit <nurseitbakytbekov@gmail.com>
2023-04-15 01:16:20 +0600

    add remove button, add check choosed category, add circle color(buisness-blue, personal-pink)

--- index.js
+++ index.js
@@ -1,44 +1,75 @@
 //Запрашивает имя пользователя
-// function recursivePrompt() {
-//   let userName = prompt("Как вас зовут?");
+function recursivePrompt() {
+  let userName = prompt("Как вас зовут?");
     
-//     if (userName === null || userName === "") {
-//       recursivePrompt();
-//     } else {
-//       changeUserName(userName);
-//     }
-//   }
-
-//   function changeUserName(userName){
-//     document.getElementById("user__name").textContent = userName;
-// }
-//   recursivePrompt();
-
-//Добавляет задачу 
-function addTask(){
-  const input=document.getElementById("input__text");
-  const text = document.getElementById("input__text").value;    //получаем значение из поля ввода задачи
-  if(text.trim().length === 0){                                 //проверка если поле Input пустое
-    input.style.border="1px solid red";
-  }
-  else{
-    input.style.border="initial";
-    const taskList=document.getElementById("task__list");
-    const newTask= document.createElement('div');
-    newTask.className="task__list__item";      
-    newTask.innerHTML=text;
-    
-    const taskEdit=document.createElement('button');
-    taskEdit.className="task__button__edit";
-    taskEdit.innerHTML="Edit";
-    const taskRemove=document.createElement('button');
-    taskRemove.className="task__button__remove";
-    taskRemove.innerHTML="Remove";
-
-    taskList.append(newTask);
-    newTask.append(taskEdit);
-    newTask.append(taskRemove);
+    if (userName === null || userName === "") {
+      recursivePrompt();
+    } else {
+      changeUserName(userName);
+    }
   }
 
+  function changeUserName(userName){
+    document.getElementById("user__name").textContent = userName;
 }
+  recursivePrompt();
+
+
+
+function addTask() {
+  const category = document.querySelector('input[name="purpose"]:checked');     // Получаем значение выбранной категории
+
+  if (category === null) {                         // Проверяем, что хотя бы одна категория выбрана                       
+    alert('Please select a category');
+    return;
+  }
+
+  const input = document.getElementById("input__text");
+  const text = document.getElementById("input__text").value.trim(); 
+  if (text.length === 0) {                                // Проверяем на пустоту строки
+    input.style.border = "1px solid red";
+  } else {
+    input.style.border = "initial";
+    const taskList = document.getElementById("task__list");
+    const newTask = document.createElement("div");
+    newTask.className = "task__list__item";
+    newTask.innerHTML = text;
+
+    const taskButtons = document.createElement("div");
+    taskButtons.className = "task__buttons";
+
+    const taskEdit = document.createElement("button");        //Создаем EDIT кнопку
+    taskEdit.classList.add("task__button__edit");
+    taskEdit.innerHTML = "Edit";
+    taskButtons.appendChild(taskEdit);
+
+    const taskRemove = document.createElement("button");       //Создаем REMOVE кнопку
+    taskRemove.classList.add("task__button__remove");
+    taskRemove.innerHTML = "Remove";
+    taskRemove.onclick = function () {
+      const taskItem = this.parentNode.parentNode; // ищем родительский элемент списка задач
+      taskList.removeChild(taskItem);
+    };
+    taskButtons.appendChild(taskRemove);
+
+    newTask.appendChild(taskButtons);
+
+    const pickBusiness = document.getElementById("pick__category__buisness");
+    const pickPersonal = document.getElementById("pick__category__personal");
+
+    if (pickBusiness.checked) {
+      newTask.classList.add("task__list__item--business");
+    } else if (pickPersonal.checked) {
+      newTask.classList.add("task__list__item--personal");
+    }
+
+    taskList.appendChild(newTask);
+    input.value = "";
+    
+  }
+}
+
+
+
+
 
